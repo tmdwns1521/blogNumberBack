0b5545ff455e041d9333a87c066eee78db608500
SENGJUN <tmdwns1521@naver.com>
2023-07-13 20:33:08 +0900

    컬쳐랜드 결제 완료

--- src/afreeca/selenium.js
+++ src/afreeca/selenium.js
@@ -18,8 +18,7 @@ export const createDriver = async () => {
       new chrome.Options()
         .headless()
         .addArguments('--disable-gpu', 'lang=ko_KR')
-    ).setChromeService(new chrome.ServiceBuilder('/usr/bin/chromedriver'))
-    .build();
+    ).build();
 
   return driver;
 };
